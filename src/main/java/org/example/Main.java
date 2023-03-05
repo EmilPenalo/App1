9abbcfbf6a6dead337df2552d506b8651884a81b
EmilPenalo <egpc0001@ce.pucmm.edu.do>
2023-03-05 17:56:06 -0400

    Added Shadowjar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,25 @@
-package org.example;public class Main {
+package org.example;
+
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        var app = Javalin.create(javalinConfig -> {
+            javalinConfig.staticFiles.add( staticFileConfig -> {
+                staticFileConfig.hostedPath = "/";
+                staticFileConfig.directory = "/public";
+                staticFileConfig.location = Location.CLASSPATH;
+            });
+        });
+
+        app.start(7000);
+
+        app.routes(() -> {
+            app.get("/", ctx -> {
+                ctx.result("Aplicacion 2");
+            });
+        });
     }
 }
